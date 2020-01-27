d418a18a94e1abaa781fbaf852e3efcdbd7bfa76
rajqumar <rajkumar@tersesoft.com>
2020-01-27 19:29:53 +0530

    d

--- src/library/config.js
+++ src/library/config.js
@@ -1,6 +1,7 @@
 export const config = {
     api: 'http://d6cebba0.ngrok.io',
-    django_api: 'http://d6cebba0.ngrok.io/api/temp/'
+    // django_api: 'http://d6cebba0.ngrok.io/api/temp/',
+    django_api: 'http://todo-git-ftp-backend.apps.us-east-1.starter.openshift-online.com/api/todo/'
 }
 
 export default config
